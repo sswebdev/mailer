7f9ca13834c3a019cdabea71582b6576c5ea4e45
Vincent Cabansag <vcabansag@gmail.com>
2013-11-25 15:17:09 -0600

    Enabling emails in development. Don't do this.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Mailer::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: 'patrick@starterleague.com',
+  password: 'rakedbmigrate',
+  address: 'smtp.gmail.com',
+  port: 587,
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
