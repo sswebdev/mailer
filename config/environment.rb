c05402a671669fa0d86cd4d8627d566a02b62cf2
Vincent Cabansag <vcabansag@gmail.com>
2013-11-25 15:49:04 -0600

    Protecting credentials.

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Mailer::Application.initialize!
 
+CREDENTIALS = YAML.load_file(Rails.root.join('config', 'email.yml'))
+
 ActionMailer::Base.smtp_settings = {
-  user_name: 'patrick@starterleague.com',
-  password: 'rakedbmigrate',
+  user_name: CREDENTIALS['username'],
+  password: CREDENTIALS['password'],
   address: 'smtp.gmail.com',
   port: 587,
   authentication: 'plain',
